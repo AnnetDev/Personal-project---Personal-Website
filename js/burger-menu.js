d4c23493e30c8743963e34ccbaf97b773f3d0b0a
Anna <annabaidikova92@gmail.com>
2025-09-08 13:42:16 +0200

    fix ways to imgs, add burger-menu-function

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -2,13 +2,33 @@ export const toggleBurgerMenu = () => {
     const menuButton = document.querySelector('[data-id="burger-menu"]');
     const headerNav = document.querySelector('[data-id="header-nav"]');
     const pageBody = document.querySelector('body');
+    const headerMenu = document.querySelector('[data-id="header-menu"]');
 
-    document.addEventListener("DOMContentLoaded", () => {
-        menuButton.addEventListener('click', () => {
-            headerNav.classList.toggle('open');
+    function closeMenu () {
+        headerNav.classList.remove('open');
+        pageBody.classList.remove('overlay-active');
+        pageBody.classList.remove('overlay-no-scroll');
+        menuButton.classList.remove('open');
+    }
+    
+    function toggleMenu () {
+        headerNav.classList.toggle('open');
             pageBody.classList.toggle('overlay-active');
             pageBody.classList.toggle('overlay-no-scroll');
             menuButton.classList.toggle('open');
+    }
+
+    document.addEventListener("DOMContentLoaded", (event) => {
+        menuButton.addEventListener('click', () => {
+            event.stopPropagation();
+            toggleMenu();
+        });
+
+        headerMenu.addEventListener('click', (event) => {
+            event.stopPropagation();
+            if (event.target.tagName === 'A') {
+                closeMenu();
+            }
         });
     });
 };
