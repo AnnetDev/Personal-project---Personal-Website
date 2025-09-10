9d14a50bc0b23584cc6abd95b9e7f626da329d92
Anna <annabaidikova92@gmail.com>
2025-09-10 12:54:28 +0200

    add styles for tablet

--- js/main.js
+++ js/main.js
@@ -6,8 +6,8 @@ AOS.init(
     {
         startEvent: 'DOMContentLoaded',
         once: true,
-        // debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
-        // throttleDelay: 99,
+        debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+        throttleDelay: 99,
         offset: 0,
         easing: 'ease',
         duration: 1400,
